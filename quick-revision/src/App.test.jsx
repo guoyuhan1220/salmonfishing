0ed63b17e1e83115089226559849cbfc25f2d232
guoyuhan1220 <guoyuhan.dalian@gmail.com>
2025-07-22 23:11:08 -0700

    remove quick-revision project files

--- quick-revision/src/App.test.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-
-// Simple test to check if the App renders without crashing
-const div = document.createElement('div')
-const root = createRoot(div)
-root.render(<App />)
-console.log('App rendered successfully!')
